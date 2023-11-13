c36d1b40813b36163840caa6d702b4aec9029b12
justingp45 <31289947+justingp45@users.noreply.github.com>
2023-11-12 21:37:19 -0800

    Results screen updated; almost properly working

--- src/screens/QuizScreen.js
+++ src/screens/QuizScreen.js
@@ -5,6 +5,11 @@ import { useRoute } from '@react-navigation/native';
 function QuizScreen ({ navigation }) { 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, updateScore] = useState(0);
+  const [count, updateCount] = useState(0);
+  const [word1, updateWord1] = useState("You got isa correct!");
+  const [word2, updateWord2] = useState("You got tatlo correct!");
+  const [word3, updateWord3] = useState("You got apat correct!");
+  const [word4, updateWord4] = useState("You got dalawa correct!");
 
   const questions = [
     {
@@ -35,14 +40,29 @@ function QuizScreen ({ navigation }) {
 
   const handleAnswer = (answer) => {
     const isCorrect = answer === questions[currentQuestion].correctAnswer;
+    updateCount(count + 1);
     if (isCorrect) {
       updateScore(score + 1);
+    }
+    if (!isCorrect) {
+        if (count == 0){
+          updateWord1("You need to study isa!");
+        }
+        if (count == 1){
+          updateWord2("You need to study tatlo!");
+        }
+        if (count == 2){
+          updateWord3("You need to study apat!");
+        }
+        if (count == 3){
+          updateWord4("You need to study dalawa!");
+        }
     }
     nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
-      navigation.navigate('Results', { score })
+      navigation.navigate('Results', { score, word1, word2, word3, word4 })
     }
   };
 
@@ -53,6 +73,11 @@ function QuizScreen ({ navigation }) {
         <Button key={index} title={option} onPress={() => handleAnswer(option)} />
       ))}
       <Text>Score: {score}</Text>
+      <Text>count value: {count}</Text>
+      <Text>word1: {word1}</Text>
+      <Text>word2: {word2}</Text>
+      <Text>word3: {word3}</Text>
+      <Text>word4: {word4}</Text>
     </View>
   );
 };
--- src/screens/Score.js
+++ src/screens/Score.js
@@ -5,9 +5,18 @@ import { useRoute } from '@react-navigation/native';
 function ResultsScreen ({ navigation }) { 
   const route = useRoute();
   const score = route.params?.score
+  const word1 = route.params?.word1
+  const word2 = route.params?.word2
+  const word3 = route.params?.word3
+  const word4 = route.params?.word4
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Your score is: {score}</Text>
+      <Text>Results:</Text>
+      <Text>{word1}</Text>
+      <Text>{word2}</Text>
+      <Text>{word3}</Text>
+      <Text>{word4}</Text>
       <Button title="Go back to Topics" onPress={() => navigation.navigate('Topics')} />
     </View>
   );
