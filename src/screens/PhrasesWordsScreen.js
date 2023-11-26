aa569fddc9552aab8b923376489872837e4a3074
justingp45 <31289947+justingp45@users.noreply.github.com>
2023-11-25 18:11:40 -0800

    Started work on new screen

---
+++ src/screens/PhrasesWordsScreen.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+function PhrasesWordsScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>List of Words:</Text>
+        <Text>isa = one</Text>
+        <Text>dalawa = two</Text>
+        <Text>tatlo = three</Text>
+        <Text>apat = four</Text>
+        <Button title="Start Quiz" onPress={() => navigation.navigate('Quiz')} />
+        <Button title="Go back to Topics" onPress={() => navigation.navigate('Topics')} />
+      </View>
+    ); 
+  };
+
+export default PhrasesWordsScreen;
